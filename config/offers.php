2cb1f134ee02e4b6c92d3d23ad7b71544fbaac27
Krunal <kp4net@gmail.com>
2025-12-23 23:53:38 +0530

    Added offers data in config

---
+++ config/offers.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+	'OFR001' => array(
+		'discount'     => 10,
+		'min_distance' => 70,
+		'max_distance' => 200,
+		'min_weight'   => 0,
+		'max_weight'   => 200,
+	),
+	'OFR002' => array(
+		'discount'     => 7,
+		'min_distance' => 100,
+		'max_distance' => 250,
+		'min_weight'   => 50,
+		'max_weight'   => 150,
+	),
+	'OFR003' => array(
+		'discount'     => 5,
+		'min_distance' => 10,
+		'max_distance' => 150,
+		'min_weight'   => 50,
+		'max_weight'   => 250,
+	),
+);
